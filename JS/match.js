e51930679bb706890339ae49d190bc529a857b96
Jacint Illes <admiralilles@gmail.com>
2017-10-29 10:14:03 +0100

    match.js próbálkozás

--- JS/match.js
+++ JS/match.js
@@ -1,4 +1,5 @@
-//json data 
+var code = location.href.split("=")[1];
+
 function getJson(url, callback) {
     var xhr = new XMLHttpRequest();
     xhr.open("get", url);
@@ -12,35 +13,34 @@ function getJson(url, callback) {
 
 getJson(
     "https://raw.githubusercontent.com/opendatajson/football.json/master/2017-18/it.1.json",
-    function (data) {
-        tableData = data.matches;
-        fillTable(tableData);
+    function (tableData) {
+        fillTable(tableData.rounds);
     }
 );
 
-//táblázat kitöltés
 function fillTable(rows) {
     var table = document.querySelector("table");
     var content = "";
-    for (var i = 0; i < rows.length; i++) {
-        var tr = "<tr>";
-        tr += "<td>" + (i + 1) + "</td>";
-        tr += "<td>" + rows[i].date + "</td>";
-        tr += "<td>" + rows[i].matches + "</td>";
-        tr += "<td>" + rows[i].team2 + "</td>";
-        tr += "<td>" + rows[i].score1 + "</td>";
-        tr += "<td>" + rows[i].score2 + "</td>";
-        tr += "</tr>";
-        content += tr;
+    var params = (new URL(document.location)).searchParams;
+    var codename = params.get("name");
+    var tr;
+    for (var k in rows) {
+        if (rows[k]["matches"]) {
+            for (var i = 0; i < rows[k].matches.length; i++) {
+                tr = "";
+                if (codename == rows[k].matches[i].team1.name || codename == rows[k].matches[i].team2.name) {
+                    tr += "<tr>";
+                    tr += "<td>" + (i + 1) + "</td>";
+                    tr += "<td>" + rows[k].matches[i].date + "</td>";
+                    tr += "<td>" + rows[k].matches[i].team1.name + "</td>";
+                    tr += "<td>" + rows[k].matches[i].team2.name + "</td>";
+                    tr += "<td>" + rows[k].matches[i].score1 + "</td>";
+                    tr += "<td>" + rows[k].matches[i].score2 + "</td>";
+                    tr += "</tr>";
+                    content += tr;
+                }
+            }
+        }
     }
-
     table.querySelector("tbody").innerHTML = content;
 }
-
-var tableData = [];
-
-
-
-//document.querySelector("#sort-key").addEventListener("click", nameKeySort);
-//document.querySelector("#sort-name").addEventListener("click", nameKeySort);
-//document.querySelector("#sort-code").addEventListener("click", codeSort);
